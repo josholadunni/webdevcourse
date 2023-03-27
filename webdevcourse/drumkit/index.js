19650636a89d5078a1a5498e78b7f9f7f1a8341f
josholadunni <32363532+josholadunni@users.noreply.github.com>
2023-03-27 18:01:41 +0000

    Added event listeners to all buttons

--- webdevcourse/drumkit/index.js
+++ webdevcourse/drumkit/index.js
@@ -1,1 +1,7 @@
-alert("Hello!");
+let buttonsQty = document.querySelectorAll('button').length;
+
+for (i = 0; i < buttonsQty; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+        alert("I was clicked");
+    })
+}
