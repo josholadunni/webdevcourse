8a0e2511eed46783c0628c560a1856d056684320
josholadunni <32363532+josholadunni@users.noreply.github.com>
2023-04-03 13:13:41 +0000

    Added this. to change the colour of whichever button the event listener was activated by

--- webdevcourse/drumkit/index.js
+++ webdevcourse/drumkit/index.js
@@ -4,5 +4,6 @@ var tom1Audio = new Audio('/webdevcourse/drumkit/sounds/tom-1.mp3')
 for (i = 0; i < buttonsQty; i++) {
     document.querySelectorAll('.drum')[i].addEventListener('click', function () {
         tom1Audio.play();
+        this.style.color = "white";
     })
 }
