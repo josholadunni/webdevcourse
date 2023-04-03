c5a7011c6d931b568743fec4b40e8e490d363347
josholadunni <32363532+josholadunni@users.noreply.github.com>
2023-04-03 12:49:52 +0000

    Added 'tom-1.mp3' to play on each button click

--- webdevcourse/drumkit/index.js
+++ webdevcourse/drumkit/index.js
@@ -1,7 +1,8 @@
 let buttonsQty = document.querySelectorAll('button').length;
+var tom1Audio = new Audio('/webdevcourse/drumkit/sounds/tom-1.mp3')
 
 for (i = 0; i < buttonsQty; i++) {
     document.querySelectorAll('.drum')[i].addEventListener('click', function () {
-        alert("I was clicked");
+        tom1Audio.play();
     })
 }
