95c65328a4aa8421351b5e2f6d92455d5ece8fee
josholadunni <32363532+josholadunni@users.noreply.github.com>
2023-04-25 16:57:41 +0000

    Shortened function to use one switch statement for both key presses and clicks instead of two. Switch uses function parameters instead.

--- webdevcourse/drumkit/index.js
+++ webdevcourse/drumkit/index.js
@@ -1,10 +1,18 @@
 let buttonsQty = document.querySelectorAll('button').length;
 
 for (i = 0; i < buttonsQty; i++) {
+
+
+    //Play sounds on click
+
     document.querySelectorAll('.drum')[i].addEventListener('click', function () {
-        let btnPressed = this.innerHTML;
-        switch (btnPressed) {
-            case "w":
+        let btnHTML = this.innerHTML;
+
+        makeSound(btnHTML);
+        
+        function makeSound(key) {
+            switch (key) {
+                case "w":
                 let tom1 = new Audio('sounds/tom-1.mp3');
                 tom1.play();
                 break;
@@ -34,44 +42,13 @@ for (i = 0; i < buttonsQty; i++) {
                 break;
             default:
                 break;
-        
+            }
         }
     })
 }
 
+//Play sounds on keypress
+
 document.addEventListener('keydown', function (event) {
-    console.log(event.key);
-    switch(event.key) {
-        case "w":
-            let tom1 = new Audio('sounds/tom-1.mp3');
-            tom1.play();
-            break;
-        case "a":
-            let tom2 = new Audio('sounds/tom-2.mp3');
-            tom2.play();
-            break;
-        case "s":
-            let tom3 = new Audio('sounds/tom-3.mp3');
-            tom3.play();
-            break;
-        case "d":
-            let tom4 = new Audio('sounds/tom-4.mp3');
-            tom4.play();
-            break;
-        case "j":
-            let snare = new Audio('sounds/snare.mp3');
-            snare.play();
-            break;
-        case "k":
-            let crash = new Audio('sounds/crash.mp3');
-            crash.play();
-            break;
-        case "l":
-            let kick = new Audio('sounds/kick-bass.mp3');
-            kick.play();
-            break;
-        default:
-            break;
-        
-    }
+    makeSound(event.key);
 });
