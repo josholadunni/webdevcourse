ceb312c25dddba9617db49e0a7fd5e47c1175b7e
josholadunni <32363532+josholadunni@users.noreply.github.com>
2023-05-28 17:05:10 +0000

    Added fade out and in animation to randomly selected button

--- webdevcourse/Simon game/index.js
+++ webdevcourse/Simon game/index.js
@@ -2,6 +2,13 @@ const buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 
+let userClickedPattern = [];
+
+let blueAudio = new Audio("sounds/blue.mp3");
+let greenAudio = new Audio("sounds/green.mp3");
+let redAudio = new Audio("sounds/red.mp3");
+let yellowAudio = new Audio("sounds/yellow.mp3");
+
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   return randomNumber;
@@ -11,4 +18,28 @@ let randomChosenColour = buttonColours[nextSequence()];
 
 gamePattern.push(randomChosenColour);
 
-$("#" + randomChosenColour).css("background-color", "black");
+$("#" + randomChosenColour)
+  .fadeOut(100)
+  .fadeIn(100);
+
+let userChosenColour = $(".btn").on("click", function () {
+  userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+});
+
+function playSound(name) {
+  if (name === "blue") {
+    blueAudio.play();
+  } else if (name === "green") {
+    greenAudio.play();
+  } else if (name === "yellow") {
+    yellowAudio.play();
+  } else {
+    redAudio.play();
+  }
+}
+
+// $("#level-title").on("click", function () {
+//   alert(userChosenColour);
+// });
