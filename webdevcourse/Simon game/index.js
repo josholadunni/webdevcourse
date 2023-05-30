f52bb51b1958d63fab88e15fef76088a82df19bd
josholadunni <32363532+josholadunni@users.noreply.github.com>
2023-05-30 12:40:04 +0000

    Added start function on keyPress and conditional statement to check if game has already been started. Need access to live server to test.

--- webdevcourse/Simon game/index.js
+++ webdevcourse/Simon game/index.js
@@ -45,7 +45,18 @@ function playSound(name) {
 
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
+
+const isStarted = false;
+
+$(document).on("keyPress", function () {
+  if (!isStarted) {
+    nextSequence();
+  }
+});
 
 // $("#level-title").on("click", function () {
 //   alert(userChosenColour);
