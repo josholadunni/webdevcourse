4ca449132773fdc594a133efe0ee7f14ed9230d9
JoshOladunni <josholadunni@gmail.com>
2023-08-02 00:05:44 +0100

    Syncing new machine files with older lesson files

---
+++ 3.5 Secrets Project/index.js
@@ -0,0 +1,41 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+const password = "ILoveProgramming";
+let passwordValid = false;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let passwordCheck = (req, res, next) => {
+  req.body.password === password
+    ? (passwordValid = true)
+    : (passwordValid = false);
+  next();
+};
+
+app.use(passwordCheck);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (passwordValid) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
