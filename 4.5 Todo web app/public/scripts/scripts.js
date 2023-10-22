481af1e06ac708b40175b7410d80dcee905a81d8
JoshOladunni <josholadunni@gmail.com>
2023-10-22 19:41:11 +0100

    Finished date tagline section and click between each date visual functionality. Next - displaying the 7 correct numbers in each circle.

--- 4.5 Todo web app/public/scripts/scripts.js
+++ 4.5 Todo web app/public/scripts/scripts.js
@@ -1,4 +1,62 @@
 let numberOfJobs = $("#number-of-jobs").text();
 let jobField = $("add-job-field");
 
-console.log(numberOfJobs);
+//Current date tagline
+
+const date = new Date();
+
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const month = monthNames[date.getMonth()];
+
+const day = dayNames[date.getDay()];
+
+let daySuffix = function () {
+  if (date.getDate().toString().slice(-1) == 1) {
+    return "st";
+  } else if (date.getDate().toString().slice(-1) == 2) {
+    return "nd";
+  } else if (date.getDate().toString().slice(-1) == 3) {
+    return "rd";
+  } else {
+    return "th";
+  }
+};
+
+const dayNumber = date.getDate() + daySuffix();
+
+$("#date-tagline").text(day + " " + dayNumber + " " + month);
+
+//
+
+//Click on each day to select the new day
+
+$(".days-of-week li span").each(function () {
+  $(this).on("click", function () {
+    $(".days-of-week li span").removeClass("current-day");
+    $(this).addClass("current-day");
+  });
+});
