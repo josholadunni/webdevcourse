9b32dfc24fd6ddafafacd240bec297642ba6d09a
Josh Oladunni <josh@Joshs-Macbook-Pro.local>
2024-01-02 17:15:08 +0000

    Finished 5.5

--- 5.5 REST APIs/solution.js
+++ 5.5 REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "f5f51412-6903-4a7f-9a07-8d45855aa773";
+const yourBearerToken = "9acecc7d-d228-49fe-81d6-66b8591ac8cc";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
