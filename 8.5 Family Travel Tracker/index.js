f0ab5da2846b778d337b6972aa55b0cbb6c8083b
Josh Oladunni <josholadunni@gmail.com>
2024-02-15 13:11:05 +0000

    Finished checkUserVisitedCountries function. Combining it with existing checkVisited function

--- 8.5 Family Travel Tracker/index.js
+++ 8.5 Family Travel Tracker/index.js
@@ -57,8 +57,8 @@ app.post("/add", async (req, res) => {
     const countryCode = data.country_code;
     try {
       await db.query(
-        "INSERT INTO visited_countries (country_code) VALUES ($1)",
-        [countryCode]
+        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+        [countryCode, currentUserId]
       );
       res.redirect("/");
     } catch (err) {
@@ -70,16 +70,13 @@ app.post("/add", async (req, res) => {
 });
 
 async function checkUserVisitedCountries(userID) {
-  // console.log("Checking for userID:", userID);
+  currentUserId = userID;
   try {
     const searchUser = await db.query(
-      "SELECT id, name FROM users WHERE id = $1",
+      "SELECT u.id, u.name, vc.country_code FROM users u JOIN visited_countries vc ON u.id = vc.user_id WHERE u.id = $1",
       [userID]
     );
-    // console.log("Rows:", searchUser.rows);
     return searchUser.rows;
-    // const userVisitedCountries = await db.query(
-    //   "SELECT u.id, u.name, vc.user_id, vc.country_code FROM users u JOIN visited_countries vc ON u.id = vc.user_id"
   } catch (error) {
     console.error("Error querying the database:", error);
     return [];
@@ -87,21 +84,16 @@ async function checkUserVisitedCountries(userID) {
 }
 
 app.post("/user", async (req, res) => {
-  //res.render index.ejs
-  //countries: joinedquery.countries
-
   const checkVisited = await checkUserVisitedCountries(Number(req.body.user));
 
   console.log(checkVisited);
 
-  // res.render("index.ejs", {
-  //   countries: checkVisited.countries,
-  //   total: countries.length,
-  //   users: users,
-  //   color: "teal",
-  // });
-
-  // console.log(req.body.user);
+  res.render("index.ejs", {
+    countries: checkVisited.countries,
+    total: countries.length,
+    users: users,
+    color: "teal",
+  });
 });
 
 app.post("/new", async (req, res) => {
