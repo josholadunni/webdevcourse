f57e48e82d63067802cbb62935c211458460ff6e
Josh Oladunni <josholadunni@gmail.com>
2024-02-27 23:51:33 +0000

    Finished setting up adding book, rendering covers and basic styling. Next - adding notes page and further styling

--- Book notes/index.js
+++ Book notes/index.js
@@ -19,10 +19,52 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  res.render("index.ejs");
+app.use(async (req, res, next) => {
+  req.user = await db.query("SELECT * FROM users WHERE user_id = 1");
+  req.user = req.user.rows[0];
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+let getBooks = async () => {
+  const result = await db.query("SELECT * FROM books");
+  return result.rows;
+};
+
+// let bookCover = () => {
+//   return;
+// };r
+
+app.get("/", async (req, res) => {
+  try {
+    let result = await getBooks();
+    res.render("index.ejs", {
+      books: await getBooks(),
+      user: req.user,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
+
+app.post("/add-book", (req, res) => {
+  try {
+    let { bookTitle, bookRating, bookDate, bookDesc, bookISBN } = req.body;
+    const [day, month, year] = bookDate.split("/");
+    const date = new Date(`${year}-${month}-${day}`);
+    db.query(
+      "INSERT INTO books(user_id, title, rating, description, date_read, isbn) VALUES ($1, $2, $3, $4, $5, $6)",
+      [req.user.user_id, bookTitle, bookRating, bookDesc, date, bookISBN]
+    );
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+try {
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+} catch (error) {
+  console.error(error);
+}
